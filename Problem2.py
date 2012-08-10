bb49ba1c6cc75ee0875adec2f070302707f49d52
liu chao <liuchao5955@163.com>
2012-08-10 19:07:39 +0800

    first commit

---
+++ Problem2.py
@@ -0,0 +1,20 @@
+'''
+Created on 2012-8-10
+
+@author: Administrator
+'''
+i = -123
+def method(n):
+    result = 0
+    if n < 0:
+        n = n * -1
+        while n != 0:
+            result = result * 10 + n % 10
+            n /= 10
+        return result * -1    
+    while n != 0:
+        result = result * 10 + n % 10
+        n /= 10
+    return result
+print method(i)
+        
